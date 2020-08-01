b64e5c6aba5673aab08f6788088c513bc1c0fb8d
wangwu <ww5201314>
2020-08-02 00:24:35 +0800

    添加多线程购票代码

--- src/main/java/com/wwcc/WWCCApplication.java
+++ src/main/java/com/wwcc/WWCCApplication.java
@@ -2,6 +2,8 @@ package com.wwcc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import tk.mybatis.spring.annotation.MapperScan;
@@ -10,7 +12,14 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan(basePackages = "com.wwcc.mapper")
 @ComponentScan(basePackages ={"com.wwcc.service","com.wwcc.controller"} )
 @SpringBootApplication
-public class WWCCApplication {
+public class WWCCApplication extends SpringBootServletInitializer {
+
+    //以下的方法必须加，部署在服务器当作servlet访问
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(WWCCApplication.class);
+    }
+
     public static void main(String[] args){
         SpringApplication.run(WWCCApplication.class,args);
     }
