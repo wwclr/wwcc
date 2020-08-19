a5872d862e2278b5493d5fef10f8890b145aa998
wangwu <wangwu@ngarihealth.com>
2020-08-19 14:12:07 +0800

    通过aspect切面来捕获调用方法，通过druid的filter来过滤所有sql，并将所有报错的sql进行输出

--- src/main/java/com/wwcc/utils/ContextUtils.java
+++ src/main/java/com/wwcc/utils/ContextUtils.java
@@ -8,24 +8,23 @@ import java.util.Map;
  * Created by wangwu on 2020/8/10.
  */
 public class ContextUtils {
-        public  static ThreadLocal<Map<String,Object>> context=new ThreadLocal<Map<String,Object>>();
-        public  static ThreadLocal<Object> context1=new ThreadLocal<Object>();
-        public  static Map<String,Object> map= Maps.newConcurrentMap();
+        public final static ThreadLocal<Map<String,Object>> context=new ThreadLocal<Map<String,Object>>();
+        public final static Map<String,Object> map= Maps.newConcurrentMap();
 
 
         public static ThreadLocal<Map<String, Object>> getContext() {
                 return context;
         }
 
-        public static void setContext(ThreadLocal<Map<String, Object>> context) {
-                ContextUtils.context = context;
-        }
+        //public static void setContext(ThreadLocal<Map<String, Object>> context) {
+        //        ContextUtils.context = context;
+        //}
 
         public static Map<String, Object> getMap() {
                 return map;
         }
 
-        public static void setMap(Map<String, Object> map) {
-                ContextUtils.map = map;
-        }
+        //public static void setMap(Map<String, Object> map) {
+        //        ContextUtils.map = map;
+        //}
 }
