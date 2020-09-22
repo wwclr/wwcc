3f3eb331ba0cfeab2979904c22525518d229a602
wangwu <wangwu@ngarihealth.com>
2020-09-22 16:31:04 +0800

    钉钉开发

--- src/main/java/com/wwcc/controller/DubboController.java
+++ src/main/java/com/wwcc/controller/DubboController.java
@@ -1,16 +1,17 @@
 package com.wwcc.controller;
 
 
-import com.alibaba.dubbo.config.annotation.Reference;
-import com.alibaba.fastjson.JSONObject;
+import com.dingtalk.api.DefaultDingTalkClient;
+import com.dingtalk.api.DingTalkClient;
+import com.dingtalk.api.request.OapiRobotSendRequest;
+import com.dingtalk.api.response.OapiRobotSendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-import java.util.List;
+
+import java.util.Arrays;
 
 @Controller
 @RequestMapping("/dubbo")
@@ -34,5 +35,46 @@ public class DubboController {
 
     }
 
+
+
+    @RequestMapping("/testDD")
+    public String testDD() {
+
+        DingTalkClient client = new DefaultDingTalkClient("https://oapi.dingtalk.com/robot/send?access_token=92f6518c802d12faf7ed8954af71e4c80cd725771525f3c6b45643560c4af803");
+        OapiRobotSendRequest request = new OapiRobotSendRequest();
+        request.setMsgtype("text");
+        OapiRobotSendRequest.Text text = new OapiRobotSendRequest.Text();
+        text.setContent("测试文本消息");
+        request.setText(text);
+        OapiRobotSendRequest.At at = new OapiRobotSendRequest.At();
+        at.setAtMobiles(Arrays.asList("132xxxxxxxx"));
+// isAtAll类型如果不为Boolean，请升级至最新SDK
+        at.setIsAtAll(true);
+        request.setAt(at);
+
+        request.setMsgtype("link");
+        OapiRobotSendRequest.Link link = new OapiRobotSendRequest.Link();
+        link.setMessageUrl("https://www.dingtalk.com/");
+        link.setPicUrl("");
+        link.setTitle("时代的火车向前开");
+        link.setText("这个即将发布的新版本，创始人xx称它为红树林。而在此之前，每当面临重大升级，产品经理们都会取一个应景的代号，这一次，为什么是红树林");
+        request.setLink(link);
+
+        request.setMsgtype("markdown");
+        OapiRobotSendRequest.Markdown markdown = new OapiRobotSendRequest.Markdown();
+        markdown.setTitle("杭州天气");
+        markdown.setText("#### 杭州天气 @156xxxx8827\n" +
+                        "> 9度，西北风1级，空气良89，相对温度73%\n\n" +
+                        "> ![screenshot](https://gw.alicdn.com/tfs/TB1ut3xxbsrBKNjSZFpXXcXhFXa-846-786.png)\n"  +
+                        "> ###### 10点20分发布 [天气](http://www.thinkpage.cn/) \n");
+        request.setMarkdown(markdown);
+        try {
+            OapiRobotSendResponse response = client.execute(request);
+
+        }catch (Exception e){
+            throw new IllegalArgumentException("error");
+        }
+        return "index";
+    }
 
 }
